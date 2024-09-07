f6357ddd4a448fc30440163ddcb758226cb0170a
sovannarapov <sovannarapov9@gmail.com>
2024-09-07 11:18:16 +0700

    feat: add and config dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+
+  getPort(): number {
+    return this.configService.get<number>('PORT');
   }
 }
