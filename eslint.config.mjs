ae4564cedc1e82eb39dec4100956f25780df0e8a
sovannarapov <sovannarapov9@gmail.com>
2024-12-15 16:00:02 +0700

    refactor: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -49,6 +49,8 @@ export default [
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
     },
   },
 ];
