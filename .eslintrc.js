e7dbebba2fb08b40fe6791ebeae3e47c71319ce4
sovannarapov <sovannarapov9@gmail.com>
2024-12-16 09:55:24 +0700

    refactor: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
@@ -21,7 +21,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-unused-vars': 'warn',
-    'no-undef': 'warn',
   },
 };
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -49,8 +49,6 @@ export default [
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      'no-unused-vars': 'warn',
-      'no-undef': 'warn',
     },
   },
 ];
